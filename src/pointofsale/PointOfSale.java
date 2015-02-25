189b10abbe7f21a9ae3d6f43896f8b71eb34b2b2
kowal_000 <kowal_000@JaredsAsus.attlocal.net>
2015-02-25 16:09:03 -0600

    Created Program

---
+++ src/pointofsale/PointOfSale.java
@@ -0,0 +1,17 @@
+
+package pointofsale;
+
+/**
+ *
+ * @author kowal_000
+ */
+public class PointOfSale {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
